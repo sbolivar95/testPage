5d78afda6fdcf2df875e95b5f1c337779dfed086
Sebastian <sebastianbolivar95@gmail.com>
2022-03-05 20:17:28 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const html = document.querySelector('html');
+const body = document.querySelector('#body');
+const phoneNumber = document.getElementById('number');
+const clickHome = document
+  .querySelector('#home')
+  .addEventListener('click', function () {
+    event.preventDefault();
+    console.log('Home');
+  });
+
+const clickNews = document
+  .querySelector('#news')
+  .addEventListener('click', function () {
+    event.preventDefault();
+    console.log('News');
+  });
+
+const clickLink1 = document
+  .querySelector('#link1')
+  .addEventListener('click', function () {
+    event.preventDefault();
+    window.open('https://www.instagram.com/daniela_saavedras/');
+  });
+
+const clickLink2 = document
+  .querySelector('#link2')
+  .addEventListener('click', function () {
+    event.preventDefault();
+    window.open('https://www.facebook.com/Saavedra-Tattoo-105570378726404');
+  });
+
+const clickLink3 = document
+  .querySelector('#link3')
+  .addEventListener('click', function () {
+    event.preventDefault();
+    phoneNumber.style.display = 'block';
+  });

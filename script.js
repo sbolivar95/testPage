58ee9e8687158035f187f8e097732f4d1e4dd074
Sebastian <sebastianbolivar95@gmail.com>
2022-03-07 16:35:20 -0400

    adding a map to the page

--- script.js
+++ script.js
@@ -3,18 +3,19 @@
 const html = document.querySelector('html');
 const body = document.querySelector('#body');
 const phoneNumber = document.getElementById('number');
-const clickHome = document
-  .querySelector('#home')
-  .addEventListener('click', function () {
-    event.preventDefault();
-    console.log('Home');
-  });
 
 const clickNews = document
   .querySelector('#news')
   .addEventListener('click', function () {
     event.preventDefault();
     document.getElementById('header2').scrollIntoView();
+  });
+
+const clickMaps = document
+  .querySelector('#home')
+  .addEventListener('click', function () {
+    event.preventDefault();
+    document.getElementById('map').scrollIntoView();
   });
 
 const clickLink1 = document
@@ -30,3 +31,17 @@ const clickLink2 = document
     event.preventDefault();
     window.open('https://www.facebook.com/Saavedra-Tattoo-105570378726404');
   });
+
+var map = L.map('map').setView([-17.7755912, -63.1878051], 17);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+L.marker([-17.77610928524583, -63.188661625430406])
+  .addTo(map)
+  .bindPopup(
+    '<center>Saavedra Tattoo Shop</center><center>Apartamento #301</center>'
+  )
+  .openPopup();
